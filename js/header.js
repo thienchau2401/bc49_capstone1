61fb56d6e09bd95fbca47f1bb7177aebf080bc38
thienchau2401 <minhthien240195.khtn@gmail.com>
2023-07-07 22:32:43 +0900

    add local storage

--- js/header.js
+++ js/header.js
@@ -4,6 +4,15 @@ $(function () {
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
+
+if (+window.localStorage.getItem('preferDark')) {
+  document.getElementById("myBody").classList.toggle('dark');
+}
 document.getElementById("switchButton").onclick = function(){
+  if (+window.localStorage.getItem("preferDark")) {
+    window.localStorage.setItem("preferDark", 0);
+  } else {
+    window.localStorage.setItem("preferDark", 1);
+  }
   document.getElementById("myBody").classList.toggle("dark");
 };
